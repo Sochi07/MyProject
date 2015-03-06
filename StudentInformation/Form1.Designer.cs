882851f6cfe2d5f8285ef954e8d52333cf06d5ca
sochi07 <abbas.sochi@gmail.com>
2015-03-06 10:54:56 +0600

    Initial Commit

---
+++ StudentInformation/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace StudentInformation
+{
+    partial class StudentInformationUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.regTextBox = new System.Windows.Forms.TextBox();
+            this.firstTextBox = new System.Windows.Forms.TextBox();
+            this.lastTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.retrieveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reg No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Last Name";
+            // 
+            // regTextBox
+            // 
+            this.regTextBox.Location = new System.Drawing.Point(132, 79);
+            this.regTextBox.Name = "regTextBox";
+            this.regTextBox.Size = new System.Drawing.Size(220, 20);
+            this.regTextBox.TabIndex = 4;
+            // 
+            // firstTextBox
+            // 
+            this.firstTextBox.Location = new System.Drawing.Point(132, 113);
+            this.firstTextBox.Name = "firstTextBox";
+            this.firstTextBox.Size = new System.Drawing.Size(220, 20);
+            this.firstTextBox.TabIndex = 5;
+            // 
+            // lastTextBox
+            // 
+            this.lastTextBox.Location = new System.Drawing.Point(132, 147);
+            this.lastTextBox.Name = "lastTextBox";
+            this.lastTextBox.Size = new System.Drawing.Size(220, 20);
+            this.lastTextBox.TabIndex = 6;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(28, 201);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 7;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // retrieveButton
+            // 
+            this.retrieveButton.Location = new System.Drawing.Point(157, 201);
+            this.retrieveButton.Name = "retrieveButton";
+            this.retrieveButton.Size = new System.Drawing.Size(75, 23);
+            this.retrieveButton.TabIndex = 8;
+            this.retrieveButton.Text = "Retrieve";
+            this.retrieveButton.UseVisualStyleBackColor = true;
+            this.retrieveButton.Click += new System.EventHandler(this.retrieveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(277, 201);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // StudentInformationUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 261);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.retrieveButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.lastTextBox);
+            this.Controls.Add(this.firstTextBox);
+            this.Controls.Add(this.regTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentInformationUI";
+            this.Text = "Student Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox regTextBox;
+        private System.Windows.Forms.TextBox firstTextBox;
+        private System.Windows.Forms.TextBox lastTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button retrieveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ StudentInformation/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformation
+{
+    public partial class StudentInformationUI : Form
+    {
+
+        private  Student aStudent = new Student();
+        public StudentInformationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            aStudent.regNo = regTextBox.Text;
+            aStudent.firstName = firstTextBox.Text;
+            aStudent.lastName = lastTextBox.Text;
+
+            ClearTextBox();
+
+            MessageBox.Show("Hello " + aStudent.GetFullName()
+                + "your reg no is: " + " " 
+                + aStudent.regNo);
+            
+        }
+
+        private void ClearTextBox()
+        {
+            regTextBox.Text = string.Empty;
+            firstTextBox.Text = string.Empty;
+            lastTextBox.Text = string.Empty;
+        }
+
+        private void retrieveButton_Click(object sender, EventArgs e)
+        {
+            regTextBox.Text = aStudent.regNo;
+            firstTextBox.Text = aStudent.firstName;
+            lastTextBox.Text = aStudent.lastName;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            
+             aStudent = new Student();
+
+            ClearTextBox();
+
+            MessageBox.Show("Cleared");
+        }
+    }
+}
